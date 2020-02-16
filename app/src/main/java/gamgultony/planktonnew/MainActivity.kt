149a7081a2bada9bc854f11e59852b8100294f17
gyumin kim <starbro7046@gmail.com>
2020-02-16 16:58:55 +0900

    Initial commit

---
+++ app/src/main/java/gamgultony/planktonnew/MainActivity.kt
@@ -0,0 +1,16 @@
+package gamgultony.planktonnew
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        savedInstanceState ?: supportFragmentManager.beginTransaction()
+            .replace(R.id.container,PoseActivity())
+            .commit()
+
+    }
+}
---
+++ app/src/main/java/gamgultony/planktonnew/TestActivity.kt
@@ -0,0 +1,54 @@
+package gamgultony.planktonnew
+
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.drawable.Drawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.core.content.res.ResourcesCompat
+
+
+class TestActivity : AppCompatActivity() {
+    /** Returns a resized bitmap of the drawable image.    */
+    private fun drawableToBitmap(drawable: Drawable): Bitmap {
+        val bitmap = Bitmap.createBitmap(257, 353, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+
+        drawable.setBounds(0, 0, canvas.width, canvas.height)
+
+        drawable.draw(canvas)
+        return bitmap
+    }
+
+    /** Calls the Posenet library functions.    */
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test)
+
+        val sampleImageView = findViewById<ImageView>(R.id.image)
+        val drawedImage = ResourcesCompat.getDrawable(resources, R.drawable.image, null)
+        val imageBitmap = drawableToBitmap(drawedImage!!)
+        sampleImageView.setImageBitmap(imageBitmap)
+        val posenet = Posenet(this.applicationContext)
+        val person = posenet.estimateSinglePose(imageBitmap)
+
+        // Draw the keypoints over the image.
+        val paint = Paint()
+        paint.color = Color.RED
+        val size = 2.0f
+
+        val mutableBitmap = imageBitmap.copy(Bitmap.Config.ARGB_8888, true)
+        val canvas = Canvas(mutableBitmap)
+        for (keypoint in person.keyPoints) {
+            canvas.drawCircle(
+                keypoint.position.x.toFloat(),
+                keypoint.position.y.toFloat(), size, paint
+            )
+        }
+        sampleImageView.adjustViewBounds = true
+        sampleImageView.setImageBitmap(mutableBitmap)
+    }
+}
